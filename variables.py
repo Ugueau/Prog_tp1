ffd05e4ed86cc98ffd5ac4b76406683f54248a76
MaelChalon <97522929+MaelChalon@users.noreply.github.com>
2024-09-16 09:27:15 +0200

    exo4 python

---
+++ variables.py
@@ -0,0 +1,19 @@
+int:int = 1
+float:float = 1.1
+str:str = "A"
+bool:bool = True
+list:list = ["AZEAZEA", 1, True, 3.4]
+tuple:tuple = ("azeaeae", 1, True)
+dict:dict = {
+    "Hugo" : "Millot",
+    "age" : 21,
+    "sexe" : 'M'
+}
+
+print(int)
+print(float)
+print(str)
+print(bool)
+print(list)
+print(tuple)
+print(dict)
