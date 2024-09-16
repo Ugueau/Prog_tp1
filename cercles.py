c43f28d65fab154847e43c20e0e73bc24e44602d
MaelChalon <97522929+MaelChalon@users.noreply.github.com>
2024-09-16 09:04:47 +0200

    exo2 python

---
+++ cercles.py
@@ -0,0 +1,13 @@
+import math
+
+radius:float = float(input("Enter the radius of the circle: "))
+
+area:float = math.pi * radius * radius
+
+perimeter:float = math.pi * radius * 2
+
+print("area = ", area, ", perimeter = ", perimeter)
+
+
+
+
