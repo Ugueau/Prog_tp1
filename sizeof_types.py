f94f406f6f0e5724465b546f2c3baf5d219830b4
MaelChalon <97522929+MaelChalon@users.noreply.github.com>
2024-09-16 09:14:38 +0200

    exo3 python

---
+++ sizeof_types.py
@@ -0,0 +1,10 @@
+import sys
+
+
+print("int : ", sys.getsizeof(int))
+print("float : ", sys.getsizeof(float))
+print("bool : ", sys.getsizeof(bool))
+print("str : ", sys.getsizeof(str))
+print("list : ", sys.getsizeof(list))
+print("tuple : ", sys.getsizeof(tuple))
+print("dict : ", sys.getsizeof(dict))
