3ae53a4a62103326b6bdce252bfb321cdf18c77a
MaelChalon <97522929+MaelChalon@users.noreply.github.com>
2024-09-16 11:06:54 +0200

    exo7 python

---
+++ controle.py
@@ -0,0 +1,26 @@
+
+# on créé 3 liste pour stocker les résutats de chaque condition et ne faire qu'une seule boucle 
+liste_controle_1:list = []
+liste_controle_2:list = []
+liste_controle_3:list = []
+
+
+for i in range(1,1001):
+    # avec les modulos on regarde si le reste de la division de i par 4 = 0 et i par 6 != 0
+    if (i % 4 == 0 ) and (i % 6 != 0): 
+        liste_controle_1.append(i)
+
+    # avec les modulos on regarde si le reste de la division de i par 8 = 0 et i par 2 = 0
+    if (i % 8 == 0) and (i % 2 == 0):
+        liste_controle_2.append(i)
+    
+    # avec les modulos on regarde si le reste de la division de i par 4 = 0 et i par 6 != 0
+    if ((i % 5 == 0) or (i % 7 == 0)) and (i % 10 != 0):
+        liste_controle_3.append(i)
+
+print("liste des nombre divisible par 4 mais pas par 6:")
+print(liste_controle_1)
+print("liste des nombre divisible par 8 et paire:")
+print(liste_controle_2)
+print("liste des nombre divisible par 5 ou 7 mais pas par 10:")
+print(liste_controle_3)
--- triangle.py
+++ triangle.py
@@ -7,16 +7,18 @@ try:
 except:
   print("You have to enter a number")
 
+# si le nombre siasie est supérieur ou égale a 10 on renvoie une erreur 
 if size >= 10 :
   print("You number must be lower than 10")
 else:
+  # boucle de 1 a size + 1 pour faire les lignes du triangle
   for i in range(1,size+1):
+    # bloucle de 1 a i+1 pour print le bon nombre d'élément dans chaque lignes
     for j in range(1,i+1):
+        # si on affiche le dernière élément de la ligne ou le premier ou que nous sommes sur la dernière ligne on affiche * sans retour a la ligne
         if (i == j) or (j == 1) or (i == size):
           print("*", end= ' ')
-        else:
+        else: # sinon on affiche # sans retour a la ligne
           print('#', end=' ')
 
-        if i == j:
-          break
-    print('')
+    print('')  # on fait un print pour retourner a la ligne
