d9b88ddc6ff305c8e00f00517526d0d8349e3e18
MaelChalon <97522929+MaelChalon@users.noreply.github.com>
2024-09-16 09:37:47 +0200

    exo5 python

---
+++ operateurs.py
@@ -0,0 +1,12 @@
+a:int = 16
+b:int = 3
+
+print ("a =", a, "et b =", b )
+
+print("a + b =",a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a % b =", a % b)
+print("a == b", a == b)
+print("a > b", a > b)
